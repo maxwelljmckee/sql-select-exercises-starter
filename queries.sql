3158d2aa2c5e969a05ecedb7efd7fd65c556ca99
Mathias <mathiasanderson88@gmail.com>
2020-10-13 15:15:17 -0700

    Finish phases, on to bonus

--- queries.sql
+++ queries.sql
@@ -194,6 +194,8 @@ WHERE (population_estimate_2018 - population_census_2010) > 200000;
 */
 
 -- your query here
+SELECT * FROM airports
+INNER JOIN cities ON airports.city_id = cities.id;
 
 \echo ========= Problem 4.2 ====================================================
 \echo
@@ -206,6 +208,11 @@ WHERE (population_estimate_2018 - population_census_2010) > 200000;
 */
 
 -- your query here
+COUNT(
+SELECT airports.name FROM airports
+INNER JOIN cities ON airports.city_id = cities.id
+WHERE city = 'New York'
+);
 
 --------------------------------------------------------------------------------
 ---- Bonuses:
